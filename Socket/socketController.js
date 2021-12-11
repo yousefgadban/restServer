33e64f661e26f1baccb6354cb62bb4fd13f0d43a
Yousef Gadban <yousefgadban@Yousefs-MacBook-Pro.local>
2021-12-11 21:57:11 +0200

    final

--- Socket/socketController.js
+++ Socket/socketController.js
@@ -2,8 +2,8 @@ const logger = require('../Logs/Logger');
 
 const start = (io) => {
     io.on('connection', socket => {
-        // console.log('New connection ' + socket.id);
-        logger.writeToLogFile('socket', 'New connection ' + socket.id);
+         console.log('New connection ' + socket.id);
+        //logger.writeToLogFile('socket', 'New connection ' + socket.id);
     
         socket.emit('message', 'Welcome');
 
